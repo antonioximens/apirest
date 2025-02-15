ed5ba9d2b59ac6d6221d545f2ab26a11010c5197
Antônio Ximenes <almeid.ximenes@gmail.com>
2025-02-15 12:58:25 -0300

    adicionando função de deletar um genero

--- src/controller/games-controller.js
+++ src/controller/games-controller.js
@@ -109,5 +109,24 @@ module.exports = {
         games[gameIndex].genres.push(genre)
 
         res.json(games[gameIndex])
+    },
+    // DELETE /games/:id/genres/:name
+    removeGenre: (req, res) => {
+        const { id, name } = req.params
+        
+        const gameIndex = games.findIndex(game => game.id === +id)
+
+        if (gameIndex === -1) {
+        return res.status(404).json({ message: 'Game not found!' })
+        }
+
+        if (!name || typeof name !== 'string' || !games[gameIndex].genres.includes(name)) {
+        return res.status(400).json({ message: 'Invalid genre!' })
+        }
+
+        games[gameIndex].genres = games[gameIndex].genres.filter(genre => genre !== name)
+
+        res.status(204).end()
     }
 }
+
